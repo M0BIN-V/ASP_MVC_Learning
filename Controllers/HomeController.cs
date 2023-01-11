9296f8447103d1700e2f5b0283bbaad4e0a42cda
MOBIN-V <mobinvalikhani@gmail.com>
2023-01-10 17:39:27 -0800

    19 . generate route url with Url.RouteUrl in home controller => Link Action

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -50,8 +50,16 @@ public class HomeController : Controller
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    [HttpGet("Error/{ErrorCode?}", Name = "ErrorAction")]
     public IActionResult Error()
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+
+    [Route("link")]
+    public IActionResult Link()
+    {
+        var url = Url.RouteUrl("ErrorAction", new { ErrorCode = 404 }, Request.Scheme);
+        return Content(url);
     }
 }
